f08f15a3a9d1ac590efc38f4c41497660ed659c7
muosilva <murilolives20@gmail.com>
2026-09-07 20:36:18 -0300

    fix:return-messages

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,9 +1,7 @@
 package handler
 
 import (
-	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/muosilva/url-shortener/internal/domain"
@@ -22,20 +20,31 @@ func NewHandler(svc service.Service) *Handler {
 
 func (h *Handler) CreateURL(w http.ResponseWriter, r *http.Request) {
 	var req domain.CreateURLRequest
-	var ctx context.Context
 
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
+		writeJSON(w, domain.JSONResponse{
+			Msg:        "Invalid JSON body",
+			StatusCode: http.StatusBadRequest,
+		})
 		return
 	}
 
 	err = req.Validation(req.Url)
 	if err != nil {
-		fmt.Println("Err: ", err)
+		writeJSON(w, domain.JSONResponse{
+			Msg:        err.Error(),
+			StatusCode: http.StatusBadRequest,
+		})
+		return
 	}
 
-	code, err := h.service.Create(ctx, req.Url)
+	code, err := h.service.Create(r.Context(), req.Url)
 	if err != nil {
+		writeJSON(w, domain.JSONResponse{
+			Msg:        "Failed to create short URL",
+			StatusCode: http.StatusInternalServerError,
+		})
 		return
 	}
 
